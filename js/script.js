c7b41f60fc916115b569f956fc4cb9474fa86479
maui30 <103097103+maui30@users.noreply.github.com>
2023-04-28 12:46:50 +0800

    fixed reference variable

--- js/script.js
+++ js/script.js
@@ -1,16 +1,2 @@
-const samp = [
-  {
-    municipality_id: 1,
-    province_id: 2,
-    municipality_name: "Adams",
-  },
-  {
-    municipality_id: 2,
-    province_id: 2,
-    municipality_name: "Bacarra",
-  },]
-
-console.log(samp[0])
-
 
 console.log(city[0])
